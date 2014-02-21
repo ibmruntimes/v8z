c43b46a76a671084ae3f00ee27fad81f7217b3d9
Junliang Yan <jyan@ca.ibm.com>
2014-02-21 12:06:14 -0500

    testing for SS5 and SSE format

--- test/cctest/test-assembler-s390.cc
+++ test/cctest/test-assembler-s390.cc
@@ -1145,6 +1145,11 @@ TEST(13) {
   __ mvcp(r15, r15, r15, 0x123,              // SS4 FORMAT
           r15, 0x456);
   __ ar(r0, r0);                             // add 2 bytes to align
+  __ lmd(r15, r15, r15, 0x123,               // SS5 FORMAT
+          r15, 0x456);
+  __ ar(r0, r0);                             // add 2 bytes to align
+  __ mvcdk(r15, 0x123, r15, 0x456);          // SSE FORMAT
+  __ ar(r0, r0);                             // add 2 bytes to align
 
   bufPos += 56;
   OS::DebugBreak();
