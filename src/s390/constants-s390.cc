fdc84206ba943d56a0b2e4ac25154ff74e4e0c6c
Joran Siu <joransiu@ca.ibm.com>
2014-03-05 13:08:17 -0500

    Update reg names for sp and fp, so they show up properly in disassembler.

--- src/s390/constants-s390.cc
+++ src/s390/constants-s390.cc
@@ -42,10 +42,10 @@ namespace internal {
 // formatting. See for example the command "objdump -d <binary file>".
 const char* Registers::names_[kNumRegisters] = {
   "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7",
-  "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15",
+  "r8", "r9", "r10", "fp", "ip", "r13", "r14", "sp",
   "r16", "r17", "r18", "r19", "r20", "r21", "r22",
   "r23", "r24", "r25", "r26", "r27", "r28", "r29",
-  "r30", "fp"
+  "r30", "r31"
 };
 
 
