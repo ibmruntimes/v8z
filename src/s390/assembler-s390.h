b8e4fe1b95720c9c2ce136004b56b51bd9fdd20a
Alan Li <lialan@ca.ibm.com>
2014-03-08 22:30:12 -0500

    get rid of lwa.

--- src/s390/assembler-s390.h
+++ src/s390/assembler-s390.h
@@ -1775,7 +1775,6 @@ SS2_FORM(zap);
   void lhzux(Register dst, const MemOperand& src);
   void lwzu(Register dst, const MemOperand& src);
   void lwzux(Register dst, const MemOperand& src);
-  void lwa(Register dst, const MemOperand& src);
 
 
 
