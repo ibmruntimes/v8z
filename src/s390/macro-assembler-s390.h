b9b8c01daa9c42a387b439ab3af392c505a970df
Junliang Yan <jyan@ca.ibm.com>
2014-03-10 12:06:28 -0400

    Remove MA::And(r,r,i)

--- src/s390/macro-assembler-s390.h
+++ src/s390/macro-assembler-s390.h
@@ -302,7 +302,7 @@ class MacroAssembler: public Assembler {
   void And(Register dst, Register src);
   // Not supported on z9
   // void And(Register dst, Register src1, Register src2);
-  void And(Register dst, Register src, const Operand& opnd);
+  // void And(Register dst, Register src, const Operand& opnd);
   void Or(Register dst, Register src);
   void Or(Register dst, Register src1, Register src2);
   void Or(Register dst, const Operand& opnd);
