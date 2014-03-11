1d619cb33d77d82515c33d970880826303df2afe
Joran Siu <joransiu@ca.ibm.com>
2014-03-11 17:30:45 -0400

    Get rid of r11 and r15, as they are fp/sp on s390. PPC references to those should now be r11_p and r15_p and will be remapped appropriately

--- src/s390/assembler-s390.cc
+++ src/s390/assembler-s390.cc
@@ -146,9 +146,9 @@ Register ToRegister(int num) {
     r0,
     r1,
     r2, r3, r4, r5, r6, r7, r8, r9, r10,
-    r11, ip, r13, r14, sp,
+    fp, ip, r13, r14, sp,
     r16, r17, r18, r19, r20, r21, r22, r23, r24,
-    r25, r26, r27, r28, r29, r30, fp
+    r25, r26, r27, r28, r29, r30, r31
   };
   return kRegisters[num];
 }
