ibmruntimes/v8z

commit 7add8f84fa6f98b85b4c5e5d30a5cdb557b09ac9

update Simulator::SetS390ConditionCode()

src/s390/simulator-s390.h
@@ -275,12 +275,10 @@ class Simulator {
   bool S390InstructionDecode(Instruction *instr);
   template <typename T>
   void SetS390ConditionCode(T lhs, T rhs) {
-    if (lhs == rhs) {
-      condition_reg_ = CC_EQ;
-    } else if (lhs < rhs) {
-      condition_reg_ = CC_LT;
-    } else {
-      condition_reg_ = CC_GT;
+    condition_reg_ = 0;
+    if (lhs == rhs) condition_reg_ |= CC_EQ;
+    if (lhs <  rhs) condition_reg_ |= CC_LT;
+    if (lhs >  rhs) condition_reg_ |= CC_GT;
     }
   }
 

commit 4d391a79b95312e7f75152a3f9e78f453422113d

fix compilation error introduced by 7add8f8: update Simulator::SetS390ConditionCode()

src/s390/simulator-s390.h
@@ -279,7 +279,6 @@ class Simulator {
     if (lhs == rhs) condition_reg_ |= CC_EQ;
     if (lhs <  rhs) condition_reg_ |= CC_LT;
     if (lhs >  rhs) condition_reg_ |= CC_GT;
-    }
   }
 
   template <typename T>