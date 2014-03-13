ibmruntimes/v8z

commit b608cfe37c55c2733f63f6535ba4ee6341250a51

replace EmitVFPTruncate ppc instruction with s390.

src/s390/macro-assembler-s390.cc
@@ -2326,13 +2326,34 @@ void MacroAssembler::ConvertToInt32(Register source,
   bne(not_int32);
 }
 
-
 void MacroAssembler::EmitVFPTruncate(VFPRoundingMode rounding_mode,
                                      Register result,
                                      DwVfpRegister double_input,
                                      Register scratch,
                                      DwVfpRegister double_scratch,
                                      CheckForInexactConversion check_inexact) {
+  Condition m;
+  switch (rounding_mode) {
+    case kRoundToZero:
+      m = Condition(5);
+      break;
+    case kRoundToNearest:
+      // TODO(AlanLi): 1 or 3??
+      UNIMPLEMENTED();
+      break;
+    case kRoundToPlusInf:
+      m = Condition(6);
+      break;
+    case kRoundToMinusInf:
+      m = Condition(7);
+      break;
+  }
+  cgdbr(result, double_input);
+
+  // according to POPS Figure 19-18, condition code 3 is set if the integer
+  // overflows or underflows.
+
+  /*
   // Convert
   if (rounding_mode == kRoundToZero) {
     fctidz(double_scratch, double_input);
@@ -2374,6 +2395,7 @@ void MacroAssembler::EmitVFPTruncate(VFPRoundingMode rounding_mode,
     fcmpu(double_scratch, double_input);
     bind(&done);
   }
+  */
 }
 
 

src/s390/code-stubs-s390.cc
@@ -736,7 +736,7 @@ void FloatingPointHelper::LoadNumberAsInt32Double(MacroAssembler* masm,
                      kCheckForInexactConversion);
 
   // Jump to not_int32 if the operation did not succeed.
-  __ bne(not_int32);
+  __ b(Condition(CC_OF), not_int32);
 
   __ bind(&done);
 }
@@ -778,7 +778,7 @@ void FloatingPointHelper::LoadNumberAsInt32(MacroAssembler* masm,
                      kCheckForInexactConversion);
 
   // Jump to not_int32 if the operation did not succeed.
-  __ bne(not_int32);
+  __ b(Condition(CC_OF), not_int32);
 
   __ bind(&done);
 }
@@ -2608,7 +2608,7 @@ void BinaryOpStub::GenerateInt32Stub(MacroAssembler* masm) {
         // result does not fit in a 32-bit integer.
         Label *not_int32 = ((result_type_ <= BinaryOpIC::INT32) ?
                             &transition : &return_heap_number);
-        __ bne(not_int32);
+        __ b(Condition(CC_OF), not_int32);
 
 #if !V8_TARGET_ARCH_S390X
         // Check if the result fits in a smi.
@@ -3292,7 +3292,7 @@ void MathPowStub::Generate(MacroAssembler* masm) {
                        scratch2,
                        double_scratch,
                        kCheckForInexactConversion);
-    __ beq(&int_exponent);
+    __ b(Condition(CC_EQ | CC_LT | CC_GT), &int_exponent);
 
     if (exponent_type_ == ON_STACK) {
       // Detect square root case.  Crankshaft detects constant +/-0.5 at

src/s390/lithium-codegen-s390.cc
@@ -3456,7 +3456,7 @@ void LCodeGen::DoMathFloor(LUnaryMathOperation* instr) {
                      input,
                      scratch,
                      double_scratch0());
-  DeoptimizeIf(ne, instr->environment());
+  DeoptimizeIf(Condition(CC_OF), instr->environment());
 
   if (instr->hydrogen()->CheckFlag(HValue::kBailoutOnMinusZero)) {
     // Test for -0.
@@ -3546,7 +3546,7 @@ void LCodeGen::DoMathRound(LUnaryMathOperation* instr) {
                      double_scratch0(),
                      scratch,
                      double_scratch1);
-  DeoptimizeIf(ne, instr->environment());
+  DeoptimizeIf(Condition(CC_OF), instr->environment());
 
   if (instr->hydrogen()->CheckFlag(HValue::kBailoutOnMinusZero)) {
     // Test for -0.
@@ -4699,7 +4699,7 @@ void LCodeGen::DoDeferredTaggedToI(LTaggedToI* instr) {
                        scratch1,
                        double_scratch2,
                        kCheckForInexactConversion);
-    DeoptimizeIf(ne, instr->environment());
+    DeoptimizeIf(Condition(CC_OF), instr->environment());
 
     if (instr->hydrogen()->CheckFlag(HValue::kBailoutOnMinusZero)) {
       __ Cmpi(input_reg, Operand::Zero());
@@ -4789,7 +4789,7 @@ void LCodeGen::DoDoubleToI(LDoubleToI* instr) {
 
     // Deoptimize if we had a vfp invalid exception,
     // including inexact operation.
-    DeoptimizeIf(ne, instr->environment());
+    DeoptimizeIf(Condition(CC_OF), instr->environment());
   }
     __ bind(&done);
 }

src/s390/stub-cache-s390.cc
@@ -3637,7 +3637,8 @@ static void GenerateSmiKeyCheck(MacroAssembler* masm,
                      scratch1,
                      double_scratch1,
                      kCheckForInexactConversion);
-  __ bne(fail);
+  // EmitVFPTruncate sets condition code 3 if it fails.
+  __ b(Condition(CC_OF), fail);
 #if V8_TARGET_ARCH_S390X
   __ SmiTag(key, scratch0);
 #else

commit ee20b725d6ca989bc65de81c00c635cebbe158c5

Fix build break caused by b608cfe

src/s390/macro-assembler-s390.cc
@@ -2318,7 +2318,7 @@ void MacroAssembler::EmitVFPTruncate(VFPRoundingMode rounding_mode,
                                      Register scratch,
                                      DwVfpRegister double_scratch,
                                      CheckForInexactConversion check_inexact) {
-  Condition m;
+  Condition m = Condition(0);
   switch (rounding_mode) {
     case kRoundToZero:
       m = Condition(5);
@@ -2332,6 +2332,9 @@ void MacroAssembler::EmitVFPTruncate(VFPRoundingMode rounding_mode,
       break;
     case kRoundToMinusInf:
       m = Condition(7);
+      break;
+    default:
+      UNIMPLEMENTED();
       break;
   }
   cgdbr(m, result, double_input);