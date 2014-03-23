4abe5bfc2967a52d3acf84243d2dad9ca3b793c7
Joran Siu <joransiu@ca.ibm.com>
2014-03-22 22:32:00 -0400

    Add instr_length_at helper to Assembler

--- src/s390/assembler-s390.h
+++ src/s390/assembler-s390.h
@@ -2006,7 +2006,13 @@ SS2_FORM(zap);
     Instruction::SetInstructionBits<T>(buffer_ + pos, instr);
   }
 
+  // Decodes instruction at pos, and returns its length
+  int32_t instr_length_at(int pos) {
+    return Instruction::InstructionLength(buffer_ + pos);
+  }
+
   static Instr instr_at(byte* pc) { return Instruction::InstructionBits(pc); }
+
   static Condition GetCondition(Instr instr);
 
   static bool IsLis(Instr instr);
