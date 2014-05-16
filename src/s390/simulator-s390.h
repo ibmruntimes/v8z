441b8b5d44058139d096c3a25e5c75e1c262535b
Joran Siu <joransiu@ca.ibm.com>
2014-05-16 00:16:49 -0400

    Remove PPC floating point regs d16-d31

--- src/s390/simulator-s390.h
+++ src/s390/simulator-s390.h
@@ -137,9 +137,7 @@ class Simulator {
     kNumGPRs = 16,
     d0 = 0, d1, d2, d3, d4, d5, d6, d7,
     d8, d9, d10, d11, d12, d13, d14, d15,
-    d16, d17, d18, d19, d20, d21, d22, d23,
-    d24, d25, d26, d27, d28, d29, d30, d31,
-    kNumFPRs = 32
+    kNumFPRs = 16
   };
 
   explicit Simulator(Isolate* isolate);
