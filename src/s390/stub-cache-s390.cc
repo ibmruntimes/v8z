50ce95db1a99d0d20d103717800ff3428d29b693
Joran Siu <joransiu@ca.ibm.com>
2014-07-10 01:07:07 -0400

    Optimize 31-bit overflow detection for NumberToI

--- src/s390/stub-cache-s390.cc
+++ src/s390/stub-cache-s390.cc
@@ -3605,8 +3605,8 @@ static void GenerateSmiKeyCheck(MacroAssembler* masm,
 #if V8_TARGET_ARCH_S390X
   __ SmiTag(key, scratch0);
 #else
-  __ SmiTagCheckOverflow(scratch1, scratch0, r0);
-  __ BranchOnOverflow(fail);
+  __ Add(scratch1, scratch0, scratch0);
+  __ b(overflow, fail);
   __ LoadRR(key, scratch1);
 #endif
   __ bind(&key_ok);
