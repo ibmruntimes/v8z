a984910ef8bc02edab4c1ddf506a212e5d699e90
Nayeem Zen <nayeem.zen@mail.utoronto.ca>
2014-09-04 17:14:06 -0400

    Upstream: constants-s390.cc

--- src/s390/constants-s390.cc
+++ src/s390/constants-s390.cc
@@ -2,35 +2,12 @@
 //
 // Copyright IBM Corp. 2012-2014. All rights reserved.
 //
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-//
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above
-//       copyright notice, this list of conditions and the following
-//       disclaimer in the documentation and/or other materials provided
-//       with the distribution.
-//     * Neither the name of Google Inc. nor the names of its
-//       contributors may be used to endorse or promote products derived
-//       from this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #include "v8.h"
 
-#if defined(V8_TARGET_ARCH_S390)
+#if V8_TARGET_ARCH_S390
 
 #include "constants-s390.h"
 
@@ -68,15 +45,17 @@ const char* Registers::Name(int reg) {
   return result;
 }
 
-// Power
+
 const char* FPRegisters::names_[kNumFPRegisters] = {
     "f0", "f1", "f2", "f3", "f4", "f5", "f6", "f7",
     "f8", "f9", "f10", "f11", "f12", "f13", "f14", "f15"
 };
+
 const char* FPRegisters::Name(int reg) {
   ASSERT((0 <= reg) && (reg < kNumFPRegisters));
   return names_[reg];
 }
+
 int FPRegisters::Number(const char* name) {
   for (int i = 0; i < kNumFPRegisters; i++) {
     if (strcmp(names_[i], name) == 0) {
@@ -88,7 +67,6 @@ int FPRegisters::Number(const char* name) {
   return kNoRegister;
 }
 
-// end of Power
 
 int Registers::Number(const char* name) {
   // Look through the canonical names.
