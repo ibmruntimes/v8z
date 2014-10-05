ibmruntimes/v8z

commit 5714b69e6e8e4b9a73eb47b92edd84df62c0dc61

perf enhancement: improve CopyCharsUnsigned non-conversion cases and undo unaligned access as it gives no additional perf improvement

src/utils.h
@@ -1278,6 +1278,9 @@ INLINE(void CopyCharsUnsigned(uint16_t* dest, const uint16_t* src, int chars));
 #elif defined(V8_HOST_ARCH_MIPS)
 INLINE(void CopyCharsUnsigned(uint8_t* dest, const uint8_t* src, int chars));
 INLINE(void CopyCharsUnsigned(uint16_t* dest, const uint16_t* src, int chars));
+#elif defined(V8_HOST_ARCH_S390)
+INLINE(void CopyCharsUnsigned(uint9_t* dest, const uint8_t* src, int chars));
+INLINE(void CopyCharsUnsigned(uint16_t* dest, const uint16_t* src, int chars));
 #endif
 
 // Copy from ASCII/16bit chars to ASCII/16bit chars.
@@ -1451,6 +1454,139 @@ void CopyCharsUnsigned(uint16_t* dest, const uint16_t* src, int chars) {
     OS::MemCopy(dest, src, chars * sizeof(*dest));
   }
 }
+
+
+#elif defined(V8_HOST_ARCH_S390)
+#define CASE(n)                                 \
+  case n:                                       \
+    memcpy(dest, src, n);                       \
+    break
+void CopyCharsUnsigned(uint8_t* dest, const uint8_t* src, int chars) {
+  switch (static_cast<unsigned>(chars)) {
+    case 0:
+      break;
+    case 1:
+      *dest = *src;
+      break;
+    CASE(2);
+    CASE(3);
+    CASE(4);
+    CASE(5);
+    CASE(6);
+    CASE(7);
+    CASE(8);
+    CASE(9);
+    CASE(10);
+    CASE(11);
+    CASE(12);
+    CASE(13);
+    CASE(14);
+    CASE(15);
+    CASE(16);
+    CASE(17);
+    CASE(18);
+    CASE(19);
+    CASE(20);
+    CASE(21);
+    CASE(22);
+    CASE(23);
+    CASE(24);
+    CASE(25);
+    CASE(26);
+    CASE(27);
+    CASE(28);
+    CASE(29);
+    CASE(30);
+    CASE(31);
+    CASE(32);
+    CASE(33);
+    CASE(34);
+    CASE(35);
+    CASE(36);
+    CASE(37);
+    CASE(38);
+    CASE(39);
+    CASE(40);
+    CASE(41);
+    CASE(42);
+    CASE(42);
+    CASE(43);
+    CASE(44);
+    CASE(45);
+    CASE(46);
+    CASE(47);
+    CASE(48);
+    CASE(49);
+    CASE(50);
+    CASE(51);
+    CASE(52);
+    CASE(53);
+    CASE(54);
+    CASE(55);
+    CASE(56);
+    CASE(57);
+    CASE(58);
+    CASE(59);
+    CASE(60);
+    CASE(61);
+    CASE(62);
+    CASE(63);
+    CASE(64);
+    default:
+      memcpy(dest, src, chars);
+      break;
+  }
+}
+#undef CASE
+
+#define CASE(n)                                 \
+  case n:                                       \
+    memcpy(dest, src, n * 2);                   \
+    break
+void CopyCharsUnsigned(uint16_t* dest, const uint16_t* src, int chars) {
+  switch (static_cast<unsigned>(chars)) {
+    case 0:
+      break;
+    case 1:
+      *dest = *src;
+      break;
+    CASE(2);
+    CASE(3);
+    CASE(4);
+    CASE(5);
+    CASE(6);
+    CASE(7);
+    CASE(8);
+    CASE(9);
+    CASE(10);
+    CASE(11);
+    CASE(12);
+    CASE(13);
+    CASE(14);
+    CASE(15);
+    CASE(16);
+    CASE(17);
+    CASE(18);
+    CASE(19);
+    CASE(20);
+    CASE(21);
+    CASE(22);
+    CASE(23);
+    CASE(24);
+    CASE(25);
+    CASE(26);
+    CASE(27);
+    CASE(28);
+    CASE(29);
+    CASE(30);
+    CASE(31);
+    CASE(32);
+    default:
+      memcpy(dest, src, chars * 2);
+      break;
+  }
+}
+#undef CASE
 #endif
 
 

src/globals.h
@@ -75,11 +75,9 @@ namespace internal {
 #elif defined(__s390x__)
 #define V8_HOST_ARCH_S390 1
 #define V8_HOST_ARCH_64_BIT 1
-#define V8_HOST_CAN_READ_UNALIGNED 1
 #elif defined(__s390__)
 #define V8_HOST_ARCH_S390 1
 #define V8_HOST_ARCH_32_BIT 1
-#define V8_HOST_CAN_READ_UNALIGNED 1
 #else
 #error "Host architecture was not detected as supported by v8"
 #endif

commit 00a32db2e1720ef126531540a09340c2232620eb

Fix CopyCharsUnsigned build break caused by commit 5714b69e6e8e4b9a73eb47b92edd84df62c0dc61

src/utils.h
@@ -1279,7 +1279,7 @@ INLINE(void CopyCharsUnsigned(uint16_t* dest, const uint16_t* src, int chars));
 INLINE(void CopyCharsUnsigned(uint8_t* dest, const uint8_t* src, int chars));
 INLINE(void CopyCharsUnsigned(uint16_t* dest, const uint16_t* src, int chars));
 #elif defined(V8_HOST_ARCH_S390)
-INLINE(void CopyCharsUnsigned(uint9_t* dest, const uint8_t* src, int chars));
+INLINE(void CopyCharsUnsigned(uint8_t* dest, const uint8_t* src, int chars));
 INLINE(void CopyCharsUnsigned(uint16_t* dest, const uint16_t* src, int chars));
 #endif
 
@@ -1508,7 +1508,6 @@ void CopyCharsUnsigned(uint8_t* dest, const uint8_t* src, int chars) {
     CASE(39);
     CASE(40);
     CASE(41);
-    CASE(42);
     CASE(42);
     CASE(43);
     CASE(44);