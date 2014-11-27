ibmruntimes/v8z

commit a57cd67065ab17a5dfb07041157b84fe8dc146e7

Clean up CEntryStub

src/s390/code-stubs-s390.cc
@@ -1566,16 +1566,7 @@ void CEntryStub::Generate(MacroAssembler* masm) {
   // Call C built-in.
   __ mov(isolate_reg, Operand(ExternalReference::isolate_address(isolate())));
 
-#if ABI_USES_FUNCTION_DESCRIPTORS && !defined(USE_SIMULATOR)
-  // Native AIX/S390X Linux use a function descriptor.
- // @TODO Haven't touched this code for S390.. See if it's applicable.
-  // especially the ToRegister(ABI_TOC_REGISTER) part.
-  __ LoadP(ToRegister(ABI_TOC_REGISTER), MemOperand(r7, kPointerSize));
-  __ LoadP(ip, MemOperand(r7, 0));  // Instruction address
-  Register target = ip;
-#else
   Register target = r7;
-#endif
 
   // To let the GC traverse the return address of the exit frames, we need to
   // know where the return address is. The CEntryStub is unmovable, so

commit 71f05588a650cf914b581a15b3a7c8d4b4b5c159

Fix presubmit errors caused by a57cd67065ab17a5dfb07041157b84fe8dc146e7

src/s390/code-stubs-s390.cc
@@ -1545,12 +1545,11 @@ void CEntryStub::Generate(MacroAssembler* masm) {
     // The return value is 16-byte non-scalar value.
     // Use frame storage reserved by calling function to pass return
     // buffer as implicit first argument.
-  __ LoadRR(r4, r3);
-  __ LoadRR(r3, r2);
-  __ la(r2, MemOperand(sp, (kStackFrameExtraParamSlot + 1) * kPointerSize));
-  isolate_reg = r5;
-   }
-  else __ LoadRR(r6, r2);
+    __ LoadRR(r4, r3);
+    __ LoadRR(r3, r2);
+    __ la(r2, MemOperand(sp, (kStackFrameExtraParamSlot + 1) * kPointerSize));
+    isolate_reg = r5;
+  }
 #endif
   // Call C built-in.
   __ mov(isolate_reg, Operand(ExternalReference::isolate_address(isolate())));