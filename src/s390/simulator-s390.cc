040605a1f7597bbd538c70d6ff577bbd1e4633a7
Nayantara Prem <prem.nayantara@gmail.com>
2015-02-06 12:06:18 -0500

    Fix compiler warnings/fail

--- src/s390/simulator-s390.cc
+++ src/s390/simulator-s390.cc
@@ -4058,7 +4058,7 @@ bool Simulator::DecodeSixByteArithmetic(Instruction *instr) {
       RILInstruction* rilInstr = reinterpret_cast<RILInstruction*>(instr);
       int r1 = rilInstr->R1Value();
       int i2 = rilInstr->I2Value();
-      int isOF;
+      bool isOF = false;
       if (AFI == op) {
         // 32-bit Add (Register + 32-bit Immediate)
         int32_t r1_val = get_low_register<int32_t>(r1);
@@ -4067,12 +4067,12 @@ bool Simulator::DecodeSixByteArithmetic(Instruction *instr) {
         set_low_register(r1, alu_out);
         SetS390ConditionCode<int32_t>(alu_out, 0);
       } else if (AGFI == op) {
-        //64-bit Add (Register + 32-bit Imm)
+        // 64-bit Add (Register + 32-bit Imm)
         int64_t r1_val = get_register(r1);
         isOF = CheckOverflowForIntAdd(r1_val, i2);
         int64_t alu_out = r1_val + i2;
         set_register(r1, alu_out);
-        SetS390ConditionCode<int64_t>(alu_out, 0); 
+        SetS390ConditionCode<int64_t>(alu_out, 0);
       }
       SetS390OverflowCode(isOF);
       break;
