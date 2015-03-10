1fb91c3242abf7b98d4179b165462f0164fbb5b2
Tiffany Huang <queenian415@gmail.com>
2015-03-10 14:25:28 -0400

    Fix to use proper 64-bit extension after shift

--- src/s390/lithium-codegen-s390.cc
+++ src/s390/lithium-codegen-s390.cc
@@ -1300,6 +1300,9 @@ void LCodeGen::DoFlooringDivByPowerOf2I(LFlooringDivByPowerOf2I* instr) {
   if (divisor > 0) {
     if (shift || !result.is(dividend)) {
       __ ShiftRightArith(result, dividend, Operand(shift));
+#if V8_TARGET_ARCH_S390X
+      __ lgfr(result, result);
+#endif
     }
     return;
   }
@@ -1340,6 +1343,9 @@ void LCodeGen::DoFlooringDivByPowerOf2I(LFlooringDivByPowerOf2I* instr) {
   Label overflow_label, done;
   __ b(overflow, &overflow_label, Label::kNear);
   __ ShiftRightArith(result, result, Operand(shift));
+#if V8_TARGET_ARCH_S390X
+  __ lgfr(result, result);
+#endif
   __ b(&done, Label::kNear);
   __ bind(&overflow_label);
   __ mov(result, Operand(kMinInt / divisor));
@@ -1746,6 +1752,9 @@ void LCodeGen::DoShiftI(LShiftI* instr) {
           DeoptimizeIf(lt, instr->environment(), cr0);
         } else {
           __ ShiftRight(result, left, scratch);
+#if V8_TARGET_ARCH_S390X
+          __ lgfr(result, result);
+#endif
         }
         break;
       case Token::SHL:
@@ -1809,6 +1818,9 @@ void LCodeGen::DoShiftI(LShiftI* instr) {
               instr->can_deopt()) {
             if (shift_count != 1) {
               __ ShiftLeft(result, left, Operand(shift_count - 1));
+#if V8_TARGET_ARCH_S390X
+              __ lgfr(result, result);
+#endif
               __ SmiTagCheckOverflow(result, result, scratch);
             } else {
               __ SmiTagCheckOverflow(result, left, scratch);
