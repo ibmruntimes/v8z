16282decf0da413d781ba3d7ff080ed46d65f718
Nayantara Prem <prem.nayantara@gmail.com>
2015-03-16 14:45:13 -0400

    Fix LITTLE_ENDIAN spelling typo in macro

--- src/s390/macro-assembler-s390.h
+++ src/s390/macro-assembler-s390.h
@@ -1755,7 +1755,7 @@ class MacroAssembler: public Assembler {
   STATIC_ASSERT(kSmiTag == 0);
   STATIC_ASSERT(kSmiTagSize + kSmiShiftSize == 32);
 #endif
-#if __BYTE_ORDER == __LITTLE_ENDIA
+#if __BYTE_ORDER == __LITTLE_ENDIAN
 #define SmiWordOffset(offset) (offset + kPointerSize / 2)
 #else
 #define SmiWordOffset(offset) offset
