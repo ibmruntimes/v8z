8d37d7a58fc1fbcf5c647e0debe4ad42b694f551
Nayantara Prem <prem.nayantara@gmail.com>
2015-03-25 17:18:19 -0400

    Support conditions lesser or greater than & less than equal or greater than equal in NegateCondition(Condition cond)

--- src/s390/constants-s390.h
+++ src/s390/constants-s390.h
@@ -125,6 +125,8 @@ inline Condition NegateCondition(Condition cond) {
   case gt: return le;
   case le: return gt;
   case lt: return ge;
+  case lt|gt: return eq;
+  case le|ge: return CC_OF;
   case CC_OF: return CC_NOF;
   default:
     ASSERT(false);
