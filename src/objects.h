6a89f256aa0b72fd5430056e553b50b3e07b2773
yangguo <yangguo@chromium.org>
2015-05-12 06:03:59 -0700

    Revert of Provide accessor for object internal properties that doesn't require debugger to be active (patchset #3 id:40001 of https://codereview.chromium.org/1126103006/)
    Reason for revert:
    GC mole issues: https://chromegw.corp.google.com/i/client.v8/builders/V8%20Linux%20-%20gcmole/builds/1950/steps/GCMole%20ia32/logs/stdio
    
    Original issue's description:
    > Provide accessor for object internal properties that doesn't require debugger to be active
    >
    > Some of the DevTools' clients need to inspect JS objects without enabling debugger. This CL allows to inspect object's internal properties without enabling debugger and instantiating debug context.
    >
    > Note that now debug context can be created lazily if v8::Debug::GetDebugContext is called when there is no debug listener. This is fragile and has already resulted in some subtle error. I'm going to fix that in a separate CL.
    >
    > BUG=chromium:481845
    > LOG=Y
    >
    > Committed: https://crrev.com/bdeb0de88c8cf5f2c78f261b45314138f525110d
    > Cr-Commit-Position: refs/heads/master@{#28362}
    
    TBR=pfeldman@chromium.org,kozyatinskiy@chromium.org,yurys@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=chromium:481845
    
    Review URL: https://codereview.chromium.org/1133243002
    
    Cr-Commit-Position: refs/heads/master@{#28365}

--- src/objects.h
+++ src/objects.h
@@ -1058,7 +1058,6 @@ class Object {
   INLINE(bool IsOrderedHashSet() const);
   INLINE(bool IsOrderedHashMap() const);
   bool IsCallable() const;
-  static bool IsPromise(Handle<Object> object);
 
   // Oddball testing.
   INLINE(bool IsUndefined() const);
