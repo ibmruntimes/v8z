755f9ee1408b446a6b5f262d72e174f4b7b1015b
yangguo <yangguo@chromium.org>
2015-05-12 07:00:47 -0700

    Reland "Use function wrapper argument to expose internal arrays to native scripts."
    Review URL: https://codereview.chromium.org/1138173002
    
    Cr-Commit-Position: refs/heads/master@{#28367}

--- src/objects.h
+++ src/objects.h
@@ -1702,9 +1702,6 @@ class JSReceiver: public HeapObject {
 // Forward declaration for JSObject::GetOrCreateHiddenPropertiesHashTable.
 class ObjectHashTable;
 
-// Forward declaration for JSObject::Copy.
-class AllocationSite;
-
 
 // The JSObject describes real heap allocated JavaScript objects with
 // properties.
@@ -2165,7 +2162,6 @@ class JSObject: public JSReceiver {
   // Copy object.
   enum DeepCopyHints { kNoHints = 0, kObjectIsShallow = 1 };
 
-  static Handle<JSObject> Copy(Handle<JSObject> object);
   MUST_USE_RESULT static MaybeHandle<JSObject> DeepCopy(
       Handle<JSObject> object,
       AllocationSiteUsageContext* site_context,
