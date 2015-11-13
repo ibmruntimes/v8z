d12ce1b780b5dba35f7abf5383a64b419de14b08
Junliang Yan <jyan@ca.ibm.com>
2015-11-13 15:44:31 -0500

    [upstream] constants-s390.cc

--- src/s390/constants-s390.cc
+++ src/s390/constants-s390.cc
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "src/v8.h"
-
 #if V8_TARGET_ARCH_S390
 
 #include "src/s390/constants-s390.h"
@@ -20,41 +18,14 @@ const char* Registers::names_[kNumRegisters] = {
 };
 
 
-// List of alias names which can be used when referring to PPC registers.
-const Registers::RegisterAlias Registers::aliases_[] = {{10, "sl"},
-                                                        {11, "r11"},
-                                                        {12, "r12"},
-                                                        {13, "r13"},
-                                                        {14, "r14"},
-                                                        {15, "r15"},
-                                                        {kNoRegister, NULL}};
-
-
-const char* Registers::Name(int reg) {
-  const char* result;
-  if ((0 <= reg) && (reg < kNumRegisters)) {
-    result = names_[reg];
-  } else {
-    result = "noreg";
-  }
-  return result;
-}
-
-
-const char* FPRegisters::names_[kNumFPRegisters] = {
+const char* DoubleRegisters::names_[kNumDoubleRegisters] = {
     "f0", "f1", "f2", "f3", "f4", "f5", "f6", "f7",
     "f8", "f9", "f10", "f11", "f12", "f13", "f14", "f15"
 };
 
 
-const char* FPRegisters::Name(int reg) {
-  DCHECK((0 <= reg) && (reg < kNumFPRegisters));
-  return names_[reg];
-}
-
-
-int FPRegisters::Number(const char* name) {
-  for (int i = 0; i < kNumFPRegisters; i++) {
+int DoubleRegisters::Number(const char* name) {
+  for (int i = 0; i < kNumDoubleRegisters; i++) {
     if (strcmp(names_[i], name) == 0) {
       return i;
     }
@@ -71,15 +42,6 @@ int Registers::Number(const char* name) {
     if (strcmp(names_[i], name) == 0) {
       return i;
     }
-  }
-
-  // Look through the alias names.
-  int i = 0;
-  while (aliases_[i].reg != kNoRegister) {
-    if (strcmp(aliases_[i].name, name) == 0) {
-      return aliases_[i].reg;
-    }
-    i++;
   }
 
   // No register with the requested name found.
