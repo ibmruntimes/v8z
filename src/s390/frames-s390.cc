2838018d3639f5f0a4bcad03b7b667805c8cd48c
Junliang Yan <jyan@ca.ibm.com>
2015-11-18 13:15:04 -0500

    [upstream] frames-s390.cc

--- src/s390/frames-s390.cc
+++ src/s390/frames-s390.cc
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "src/v8.h"
-
 #if V8_TARGET_ARCH_S390
 
 #include "src/assembler.h"
@@ -11,6 +9,7 @@
 #include "src/macro-assembler.h"
 #include "src/s390/assembler-s390.h"
 #include "src/s390/assembler-s390-inl.h"
+#include "src/s390/frames-s390.h"
 #include "src/s390/macro-assembler-s390.h"
 
 namespace v8 {
