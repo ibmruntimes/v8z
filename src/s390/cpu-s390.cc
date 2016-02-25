39c4977b64c5ae0f0f0e7f4a43ddf30ddd2ebc78
Joran Siu <joransiu@ca.ibm.com>
2016-02-25 17:04:09 -0500

    Proper implementation of IFlushCache for S390
    z/Architecture provides strong memory support, which means
    IFlushCache is effectively a NOP.

--- src/s390/cpu-s390.cc
+++ src/s390/cpu-s390.cc
@@ -6,28 +6,17 @@
 #include "src/v8.h"
 
 #if V8_TARGET_ARCH_S390
-
 #include "src/assembler.h"
-#include "src/macro-assembler.h"
-#include "src/simulator.h"  // for cache flushing.
 
 namespace v8 {
 namespace internal {
 
 void CpuFeatures::FlushICache(void* buffer, size_t size) {
-  // Nothing to do flushing no instructions.
-  if (size == 0) {
-    return;
-  }
-
-#if defined(USE_SIMULATOR)
-  // Not generating S390 instructions for C-code. This means that we are
-  // building an S390 emulator based target.  We should notify the simulator
-  // that the Icache was flushed.
-  // None of this code ends up in the snapshot so there are no issues
-  // around whether or not to generate the code when building snapshots.
-  Simulator::FlushICache(Isolate::Current()->simulator_i_cache(), buffer, size);
-#endif  // USE_SIMULATOR
+  // Given the strong memory model on z/Architecture, and the single
+  // thread nature of V8 and JavaScript, instruction cache flushing
+  // is not necessary.  The architecture guarantees that if a core
+  // patches its own instruction cache, the updated instructions will be
+  // reflected automatically.
 }
 
 }  // namespace internal
