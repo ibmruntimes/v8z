c2f6102cc54d89fb17601198e6a39d2ecbb2c2bc
liKelvinKh <li.kelvin.kh@gmail.com>
2016-12-08 14:15:37 -0500

    add back semaphore files

---
+++ src/s390/semaphore-zos.h
@@ -0,0 +1,41 @@
+// Copyright 2012 the V8 project authors. All rights reserved.
+//
+// Copyright IBM Corp. 2016. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef V8_S390_SEMAPHORE_ZOS_H_
+#define V8_S390_SEMAPHORE_ZOS_H_
+
+#include <errno.h>
+#include <inttypes.h>
+#include <pthread.h>
+#include <sys/sem.h>
+
+#if __WORDSIZE == 64
+# define __SIZEOF_SEM_T 32
+#else
+# define __SIZEOF_SEM_T 16
+#endif
+
+#define SEM_FAILED ((sem_t *) 0)
+
+typedef union {
+  char __size[__SIZEOF_SEM_T];
+  int64_t __align;
+} sem_t;
+
+int sem_init(int *semid, int pshared, unsigned int value);
+
+int sem_destroy(int *semid);
+
+int sem_wait(int *semid);
+
+int sem_trywait(int *semid);
+
+int sem_post(int *semid);
+
+int sem_timedwait(int *semid, struct timespec *timeout);
+
+#endif  // V8_S390_SEMAPHORE_ZOS_H_
