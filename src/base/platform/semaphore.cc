24c4f051fb2833affdeabff47d3a5581b9acffc5
liKelvinKh <li.kelvin.kh@gmail.com>
2016-12-08 11:16:18 -0500

    [z/OS] Fix semaphore timed wait.
    Use relative time delta rather than absolute time on z/OS.
    Expect EAGAIN errno on z/OS when wait times out.
    
    https://github.com/ibmruntimes/v8z/commit/5d035b545dba4be18c12558079f5961024b32034

--- src/base/platform/semaphore.cc
+++ src/base/platform/semaphore.cc
@@ -121,10 +121,13 @@ bool Semaphore::WaitFor(const TimeDelta& rel_time) {
   } while (!timer.HasExpired(rel_time));
   return false;
 #else
+#if V8_OS_ZOS
+  struct timespec ts = rel_time.ToTimespec();
+#else
   // Compute the time for end of timeout.
   const Time time = Time::NowFromSystemTime() + rel_time;
   const struct timespec ts = time.ToTimespec();
-
+#endif
   // Wait for semaphore signalled or timeout.
   while (true) {
     int result = sem_timedwait(&native_handle_, &ts);
@@ -136,7 +139,11 @@ bool Semaphore::WaitFor(const TimeDelta& rel_time) {
       result = -1;
     }
 #endif
+#if V8_OS_ZOS
+    if (result == -1 && errno == EAGAIN) {
+#else
     if (result == -1 && errno == ETIMEDOUT) {
+#endif
       // Timed out while waiting for semaphore.
       return false;
     }
