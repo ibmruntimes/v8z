53a41c785ae04d4a5803c2c39a74c84918f2a2a5
Muntasir Mallick <mmallick@ca.ibm.com>
2017-03-31 14:19:26 -0400

    [z/OS] import platform and fix typo

--- tools/testrunner/local/commands.py
+++ tools/testrunner/local/commands.py
@@ -1,4 +1,3 @@
-/
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
 # met:
@@ -29,6 +28,7 @@ import os
 import subprocess
 import sys
 import time
+import platform
 from threading import Timer
 
 from ..local import utils
