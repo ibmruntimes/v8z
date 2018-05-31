8b1533a3c0ee1d6e23b4407207822437c709fea8
Muntasir Mallick <mmallick@ca.ibm.com>
2018-05-31 15:45:23 -0400

    [z/OS] Use custom semaphore implementation
    BSD semaphores need to be explicitly deallocated by
    a process for the operating system to reclaim it on
    USS. This leaves stale semaphores around whenever a
    v8 based application terminates abnormally.
    To address this issue we are replacing
    the use of BSD semaphores with custom semaphore
    implementation which uses pthread mutexes/condition
    variables. Any pthread mutexes/condition variables
    that are left behind by a terminated process are
    cleaned up by the operating system.

--- src/base/platform/semaphore.cc
+++ src/base/platform/semaphore.cc
@@ -111,12 +111,8 @@ void Semaphore::Wait() {
 
 bool Semaphore::WaitFor(const TimeDelta& rel_time) {
   // Compute the time for end of timeout.
-#if V8_OS_ZOS
-  struct timespec ts = rel_time.ToTimespec();
-#else  
   const Time time = Time::NowFromSystemTime() + rel_time;
   const struct timespec ts = time.ToTimespec();
-#endif
   // Wait for semaphore signalled or timeout.
   while (true) {
     int result = sem_timedwait(&native_handle_, &ts);
@@ -128,11 +124,7 @@ bool Semaphore::WaitFor(const TimeDelta& rel_time) {
       result = -1;
     }
 #endif
-#if V8_OS_ZOS
-    if (result == -1 && errno == EAGAIN) {
-#else
     if (result == -1 && errno == ETIMEDOUT) {
-#endif
         // Timed out while waiting for semaphore.
       return false;
     }
@@ -140,11 +132,6 @@ bool Semaphore::WaitFor(const TimeDelta& rel_time) {
     DCHECK_EQ(-1, result);
     DCHECK_EQ(EINTR, errno);
   }
-}
-void Semaphore::ReleaseSystemResources() {
-#if V8_OS_ZOS
-    sem_destroy_all();
-#endif
 }
 
 #elif V8_OS_WIN
