ibmruntimes/v8z

commit 12d693dee30874be1883f90bff8ba88bca03fc6f

[z/OS] Enable allocation of large objects
z/OS currently allows allocation of pages of 1MB alignment
above the bar. This is a problem for the v8 heap space
allocator because most objects are aligned at smaller
boundaries. Inevitably this leads to below the bar memory
being exhausted and any further request results in an
OOM error.

For now we will force allocation above the bar even when
the alignment is below 1MB once we have run out of
memory below the bar. Eventually we will need to implement
a memory pool manager or wait till z/OS has anonymous mmap
support.

src/base/platform/platform-zos.cc
@@ -317,15 +317,15 @@ void OS::SignalCodeMovingGC() {
 static const int kMmapFd = -1;
 // Constants used for mmap.
 static const int kMmapFdOffset = 0;
-VirtualMemory::VirtualMemory() : address_(NULL), size_(0) { }
+VirtualMemory::VirtualMemory() : address_(NULL), size_(0), reservation_(NULL) { }
 
 
 VirtualMemory::VirtualMemory(size_t size)
-    : address_(ReserveRegion(size)), size_(size) { }
+    : address_(ReserveRegion(size)), size_(size), reservation_(address_) { }
 
 
 VirtualMemory::VirtualMemory(size_t size, size_t alignment)
-    : address_(NULL), size_(0) {
+    : address_(NULL), size_(0), reservation_(NULL) {
   DCHECK((alignment % OS::AllocateAlignment()) == 0);
   //Memory pages with 1MB alignment will be allocated using __moservices
   bool rmode64 = SysInfo::ExecutablePagesAbove2GB(); 
@@ -335,6 +335,7 @@ VirtualMemory::VirtualMemory(size_t size, size_t alignment)
      if (reservation == MAP_FAILED) return;
      address_ = reservation;
      size_ = size;
+     reservation_ = address_;
      return;
   }
   size_t request_size = RoundUp(size + alignment,
@@ -343,7 +344,24 @@ VirtualMemory::VirtualMemory(size_t size, size_t alignment)
   void* reservation = anon_mmap(OS::GetRandomMmapAddr(),
                            request_size);
 
-  if (reservation == MAP_FAILED) return;
+  //Try allocating from 64-bit address space, this is a last ditch effort
+  //to allocate memory with requested size and alignment, since there will
+  //be some fragmentation by going down this path
+  if (reservation == MAP_FAILED) {
+     request_size = RoundUp(size + alignment,
+                            static_cast<intptr_t>(kMegaByte));
+     reservation = anon_mmap(OS::GetRandomMmapAddr(),
+                        request_size);
+     if (reservation == MAP_FAILED) return;
+     reservation_ = reservation;
+     
+     uint8_t * base = static_cast<uint8_t *>(reservation_);
+     uint8_t * aligned_base = RoundUp(base, alignment);
+     DCHECK_LE(base, aligned_base);
+     address_ = aligned_base;
+     size = request_size;
+     return;
+  }
 
   uint8_t* base = static_cast<uint8_t*>(reservation);
   uint8_t* aligned_base = RoundUp(base, alignment);
@@ -376,7 +394,7 @@ VirtualMemory::VirtualMemory(size_t size, size_t alignment)
 
 VirtualMemory::~VirtualMemory() {
   if (IsReserved()) {
-    bool result = ReleaseRegion(address(), size());
+    bool result = ReleaseRegion(reservation_, size());
     DCHECK(result);
     USE(result);
   }

src/base/platform/platform-posix.cc
@@ -238,7 +238,7 @@ void* OS::GetRandomMmapAddr() {
 
 
 size_t OS::AllocateAlignment() {
-  //return 1048576;
+  //return static_cast<size_t>(1048576);
     return static_cast<size_t>(sysconf(_SC_PAGESIZE));
 }
 

src/base/platform/platform.h
@@ -318,8 +318,11 @@ class VirtualMemory {
 
   // Construct a virtual memory by assigning it some already mapped address
   // and size.
+#ifdef V8_OS_ZOS
+  VirtualMemory(void* address, size_t size) : address_(address), size_(size), reservation_(address) {}
+#else
   VirtualMemory(void* address, size_t size) : address_(address), size_(size) {}
-
+#endif
   // Releases the reserved memory, if any, controlled by this VirtualMemory
   // object.
   ~VirtualMemory();
@@ -362,6 +365,9 @@ class VirtualMemory {
     size_t size = size_;
     CHECK(InVM(address, size));
     Reset();
+#ifdef V8_OS_ZOS
+    address = reservation_;
+#endif
     bool result = ReleaseRegion(address, size);
     USE(result);
     DCHECK(result);
@@ -373,6 +379,9 @@ class VirtualMemory {
     DCHECK(!IsReserved());
     address_ = from->address_;
     size_ = from->size_;
+#ifdef V8_OS_ZOS
+    reservation_ = from->reservation_;
+#endif
     from->Reset();
   }
 
@@ -401,6 +410,10 @@ class VirtualMemory {
 
   void* address_;  // Start address of the virtual memory.
   size_t size_;  // Size of the virtual memory.
+#if defined(V8_OS_ZOS)
+  void* reservation_;
+#endif
+
 };
 
 

src/heap/spaces.h
@@ -486,7 +486,11 @@ class MemoryChunk {
       + kIntptrSize               // intptr_t flags_
       + kPointerSize              // Address area_start_
       + kPointerSize              // Address area_end_
+#ifdef V8_OS_ZOS
+      + 3 * kPointerSize
+#else
       + 2 * kPointerSize          // base::VirtualMemory reservation_
+#endif 
       + kPointerSize              // Address owner_
       + kPointerSize              // Heap* heap_
       + kIntSize;                 // int progress_bar_

commit 9ba7834442acbb5400743a71300970ee6f859ec3

Ensure reservation_ is initialized correctly
Fix an issue introduced in commit 12d693dee30874be1883f90bff8ba88bca03fc6f
where reservation_ field may not be initalized with the result of a successful
anon_mmap() call.  In the deconstructor, the memory is released based on
reservation_ value.

Signed-off-by: Joran Siu <joransiu@ca.ibm.com>

src/base/platform/platform-zos.cc
@@ -328,7 +328,7 @@ VirtualMemory::VirtualMemory(size_t size, size_t alignment)
     : address_(NULL), size_(0), reservation_(NULL) {
   DCHECK((alignment % OS::AllocateAlignment()) == 0);
   //Memory pages with 1MB alignment will be allocated using __moservices
-  bool rmode64 = SysInfo::ExecutablePagesAbove2GB(); 
+  bool rmode64 = SysInfo::ExecutablePagesAbove2GB();
   if (rmode64 && size % kMegaByte == 0) {
      void * reservation = anon_mmap(OS::GetRandomMmapAddr(),
                                     size);
@@ -348,19 +348,21 @@ VirtualMemory::VirtualMemory(size_t size, size_t alignment)
   //to allocate memory with requested size and alignment, since there will
   //be some fragmentation by going down this path
   if (reservation == MAP_FAILED) {
-     request_size = RoundUp(size + alignment,
-                            static_cast<intptr_t>(kMegaByte));
-     reservation = anon_mmap(OS::GetRandomMmapAddr(),
-                        request_size);
-     if (reservation == MAP_FAILED) return;
-     reservation_ = reservation;
-     
-     uint8_t * base = static_cast<uint8_t *>(reservation_);
-     uint8_t * aligned_base = RoundUp(base, alignment);
-     DCHECK_LE(base, aligned_base);
-     address_ = aligned_base;
-     size = request_size;
-     return;
+    request_size = RoundUp(size + alignment,
+                           static_cast<intptr_t>(kMegaByte));
+    reservation = anon_mmap(OS::GetRandomMmapAddr(),
+                            request_size);
+
+    if (reservation == MAP_FAILED)
+       return;
+
+    uint8_t * base = static_cast<uint8_t *>(reservation);
+    uint8_t * aligned_base = RoundUp(base, alignment);
+    DCHECK_LE(base, aligned_base);
+    address_ = aligned_base;
+    size = request_size;
+    reservation_ = reservation;
+    return;
   }
 
   uint8_t* base = static_cast<uint8_t*>(reservation);
@@ -386,6 +388,7 @@ VirtualMemory::VirtualMemory(size_t size, size_t alignment)
 
   address_ = static_cast<void*>(aligned_base);
   size_ = aligned_size;
+  reservation_ = reservation;
 #if defined(LEAK_SANITIZER)
   __lsan_register_root_region(address_, size_);
 #endif