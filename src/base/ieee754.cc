4dc6f0722f5e91b8e9b8eff5e1f2b25a42f4291c
Shuowang (Wayne) Zhang <shuowang.zhang@ibm.com>
2019-04-11 16:56:10 -0400

    Workaround njsc++ bug in Math.cos() function

--- src/base/ieee754.cc
+++ src/base/ieee754.cc
@@ -218,7 +218,12 @@ int32_t __ieee754_rem_pio2(double x, double *y) {
       pio2_3 = 2.02226624871116645580e-21,  /* 0x3BA3198A, 0x2E000000 */
       pio2_3t = 8.47842766036889956997e-32; /* 0x397B839A, 0x252049C1 */
 
+  #ifdef V8_OS_ZOS
+  volatile double z; /* workaround for njsc++ bug */
+  double w, t, r, fn;
+  #else
   double z, w, t, r, fn;
+  #endif
   double tx[3];
   int32_t e0, i, j, nx, n, ix, hx;
   uint32_t low;
